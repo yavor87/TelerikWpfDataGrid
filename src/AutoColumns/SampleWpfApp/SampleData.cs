e8b83c5faa52349f63d9ab0dead83cc604092927
Yavor Ivanov <yavor87@gmail.com>
2018-08-29 11:50:46 +0300

    Adding sample app for Auto and Manual columns

---
+++ src/AutoColumns/SampleWpfApp/SampleData.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleWpfApp
+{
+    public class Employee
+    {
+        [Display(AutoGenerateField = false)]
+        public string FirstName { get; set; }
+
+        [Display(AutoGenerateField = false)]
+        public string LastName { get; set; }
+
+        [Display(AutoGenerateField = false)]
+        public decimal Salary { get; set; }
+
+        [Display(Order = 2)]
+        public string Department { get; set; }
+
+        [Display(Name = "Name", Order = 1)]
+        public string FullName => $"{FirstName} {LastName}";
+
+        [Display(Name = "Salary", Order = 3)]
+        public string FormattedSalary => this.Salary.ToString("C");
+    }
+
+    public class SampleData
+    {
+        public static Employee[] Employees => new Employee[]
+        {
+            new Employee()
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Salary = 25000,
+                Department = "Marketing"
+            },
+            new Employee()
+            {
+                FirstName = "Peter",
+                LastName = "Peterson",
+                Salary = 27000,
+                Department = "Marketing"
+            },
+            new Employee()
+            {
+                FirstName = "Mark",
+                LastName = "Harison",
+                Salary = 35000,
+                Department = "Engineering"
+            },
+            new Employee()
+            {
+                FirstName = "David",
+                LastName = "Peterson",
+                Salary = 38000,
+                Department = "Engineering"
+            },
+            new Employee()
+            {
+                FirstName = "Dave",
+                LastName = "Nickleson",
+                Salary = 24000,
+                Department = "Engineering"
+            }
+        };
+    }
+}
