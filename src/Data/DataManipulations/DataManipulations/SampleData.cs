2238c5679e10dddc05b56906e72d13cefeba4fb7
Yavor Ivanov <yavor87@gmail.com>
2018-11-23 16:56:55 +0200

    Data manipulations

---
+++ src/Data/DataManipulations/DataManipulations/SampleData.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace DataManipulations
+{
+    public class Employee
+    {
+        [Display(AutoGenerateField = false)]
+        public string FirstName { get; set; }
+
+        [Display(AutoGenerateField = false)]
+        public string LastName { get; set; }
+
+        [Display(AutoGenerateField = false)]
+        public decimal Salary { get; set; }
+
+        [Display(Order = 2)]
+        public string Department { get; set; }
+
+        [Display(Name = "Name", Order = 1)]
+        public string FullName => $"{FirstName} {LastName}";
+
+        [Display(Name = "Salary", Order = 3)]
+        public string FormattedSalary => this.Salary.ToString("C");
+    }
+
+    public class EmployeeGenerator
+    {
+        public EmployeeGenerator()
+        {
+            _r = new Random();
+            _generatedNames = new HashSet<string>();
+        }
+
+        private static Lazy<EmployeeGenerator> _instance = new Lazy<EmployeeGenerator>();
+        private Random _r;
+        private string[] _firstNames = new string[] { "John", "Peter", "Mark", "David", "Dave", "Adam", "Adrian", "Alan", "Alexander", "Andrew", "Anthony", "Austin", "Benjamin", "Blake", "Boris", "Brandon", "Brian", "Cameron", "Carl", "Charles", "Christian", "Christopher", "Colin", "Connor", "Dan", "David", "Dominic", "Dylan", "Edward", "Eric", "Evan", "Frank", "Gavin", "Gordon", "Harry", "Ian", "Isaac", "Jack", "Jacob", "Jake", "James", "Jason", "Joe", "John", "Jonathan", "Joseph", "Joshua", "Julian", "Justin", "Keith", "Kevin", "Leonard", "Liam", "Lucas", "Luke", "Matt", "Max", "Michael", "Nathan", "Neil", "Nicholas", "Oliver", "Owen", "Paul", "Peter", "Phil", "Piers", "Richard", "Robert", "Ryan", "Sam", "Sean", "Sebastian", "Simon", "Stephen", "Steven", "Stewart", "Thomas", "Tim", "Trevor", "Victor", "Warren", "William" };
+        private string[] _lastNames = new string[] { "Doe", "Peterson", "Harison", "Nickleson", "Abraham", "Allan", "Alsop", "Anderson", "Arnold", "Avery", "Bailey", "Baker", "Ball", "Bell", "Berry", "Black", "Blake", "Bond", "Bower", "Brown", "Buckland", "Burgess", "Butler", "Cameron", "Campbell", "Carr", "Chapman", "Churchill", "Clark", "Clarkson", "Coleman", "Cornish", "Davidson", "Davies", "Dickens", "Dowd", "Duncan", "Dyer", "Edmunds", "Ellison", "Ferguson", "Fisher", "Forsyth", "Fraser", "Gibson", "Gill", "Glover", "Graham", "Grant", "Gray", "Greene", "Hamilton", "Hardacre", "Harris", "Hart", "Hemmings", "Henderson", "Hill", "Hodges", "Howard", "Hudson", "Hughes", "Hunter", "Ince", "Jackson", "James", "Johnston", "Jones", "Kelly", "Kerr", "King", "Knox", "Lambert", "Langdon", "Lawrence", "Lee", "Lewis", "Lyman", "MacDonald", "Mackay", "Mackenzie", "MacLeod", "Manning", "Marshall", "Martin", "Mathis", "May", "McDonald", "McLean", "McGrath", "Metcalfe", "Miller", "Mills", "Mitchell", "Morgan", "Morrison", "Murray", "Nash", "Newman", "Nolan", "North", "Ogden", "Oliver", "Paige", "Parr", "Parsons", "Paterson", "Payne", "Peake", "Peters", "Piper", "Poole", "Powell", "Pullman", "Quinn", "Rampling", "Randall", "Rees", "Reid", "Roberts", "Robertson", "Ross", "Russell", "Rutherford", "Sanderson", "Scott", "Sharp", "Short", "Simpson", "Skinner", "Slater", "Smith", "Springer", "Stewart", "Sutherland", "Taylor", "Terry", "Thomson", "Tucker", "Turner", "Underwood", "Vance", "Vaughan", "Walker", "Wallace", "Walsh", "Watson", "Welch", "White", "Wilkins", "Wilson", "Wright", "Young" };
+        private HashSet<string> _generatedNames;
+        private Department[] _departments = new Department[]
+        {
+            new Department("Engineering", new Position("Engineering Expert", 20000, 30000), new Position("Senior Software Engineer", 45000, 70000), new Position("Junior Software Engineer", 15000, 25000), new Position("Software Engineer", 25000, 45000)),
+            new Department("Marketing", new Position("Marketing Expert", 15000, 25000), new Position("Senior Manager", 60000, 250000)),
+            new Department("Operations", new Position("Intern", 5000, 15000), new Position("Manager", 60000, 250000)),
+        };
+
+        public static EmployeeGenerator Instance => _instance.Value;
+
+        public Employee Generate()
+        {
+            if (_generatedNames.Count > 20)
+                _generatedNames.Remove(_generatedNames.Last());
+
+            Department department = _departments[_r.Next(0, _departments.Length)];
+            Position position = department.Positions[_r.Next(0, department.Positions.Length)];
+
+            Employee employee = new Employee();
+            do
+            {
+                employee.FirstName = _firstNames[_r.Next(0, _firstNames.Length)];
+                employee.LastName = _lastNames[_r.Next(0, _lastNames.Length)];
+            }
+            while (_generatedNames.Contains(employee.FullName));
+            _generatedNames.Add(employee.FullName);
+
+            employee.Department = department.Name;
+            employee.Salary = _r.Next(position.MinSalary, position.MaxSalary);
+            return employee;
+        }
+
+        public IEnumerable<Employee> Generate(int number)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                yield return Generate();
+            }
+        }
+
+        private class Department
+        {
+            public Department(string name, params Position[] positions)
+            {
+                this.Name = name;
+                this.Positions = positions;
+            }
+
+            public string Name { get; private set; }
+            public Position[] Positions { get; private set; }
+        }
+
+        private class Position
+        {
+            public Position(string name, int minSalary, int maxSalary)
+            {
+                Name = name;
+                MinSalary = minSalary;
+                MaxSalary = maxSalary;
+            }
+
+            public string Name { get; private set; }
+            public int MinSalary { get; private set; }
+            public int MaxSalary { get; private set; }
+        }
+    }
+
+    public class SampleData
+    {
+        public static Employee[] Employees => new Employee[]
+        {
+            new Employee()
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Salary = 25000,
+                Department = "Marketing"
+            },
+            new Employee()
+            {
+                FirstName = "Peter",
+                LastName = "Peterson",
+                Salary = 27000,
+                Department = "Marketing"
+            },
+            new Employee()
+            {
+                FirstName = "Mark",
+                LastName = "Harison",
+                Salary = 35000,
+                Department = "Engineering"
+            },
+            new Employee()
+            {
+                FirstName = "David",
+                LastName = "Peterson",
+                Salary = 38000,
+                Department = "Engineering"
+            },
+            new Employee()
+            {
+                FirstName = "Dave",
+                LastName = "Nickleson",
+                Salary = 24000,
+                Department = "Engineering"
+            }
+        };
+    }
+}
---
+++ src/Data/DataManipulations/DataManipulations/ViewModels/MainViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Telerik.Windows.Controls;
+using Telerik.Windows.Data;
+
+namespace DataManipulations.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            _data = new ObservableCollection<Employee>(SampleData.Employees);
+            _dataView = new QueryableCollectionView(_data);
+
+            AddNewItemCommand = new DelegateCommand(OnAddNewItem);
+            RemoveLastItemCommand = new DelegateCommand(OnRemoveLastItem);
+            SortDataCommand = new DelegateCommand(OnSortData);
+            FilterDataCommand = new DelegateCommand(OnFilterData);
+            GroupDataCommand = new DelegateCommand(OnGroupData);
+        }
+
+        private ObservableCollection<Employee> _data;
+        private QueryableCollectionView _dataView;
+
+        public ICommand AddNewItemCommand { get; }
+        public ICommand RemoveLastItemCommand { get; }
+        public ICommand SortDataCommand { get; }
+        public ICommand FilterDataCommand { get; }
+        public ICommand GroupDataCommand { get; }
+
+        public QueryableCollectionView Data => _dataView;
+
+        private void OnAddNewItem(object obj)
+        {
+            _data.Add(EmployeeGenerator.Instance.Generate());
+        }
+
+        private void OnRemoveLastItem(object obj)
+        {
+            if (_data.Count == 0)
+                return;
+
+            _data.RemoveAt(_data.Count - 1);
+        }
+
+        private void OnSortData(object obj)
+        {
+            if (_dataView.SortDescriptors.Count != 0)
+            {
+                _dataView.SortDescriptors.Clear();
+            }
+            else
+            {
+                _dataView.SortDescriptors.Add(new SortDescriptor() { Member = nameof(Employee.LastName), SortDirection = System.ComponentModel.ListSortDirection.Descending });
+            }
+        }
+
+        private void OnFilterData(object obj)
+        {
+            if (_dataView.FilterDescriptors.Count != 0)
+            {
+                _dataView.FilterDescriptors.Clear();
+            }
+            else
+            {
+                _dataView.FilterDescriptors.Add(new FilterDescriptor(nameof(Employee.LastName), FilterOperator.StartsWith, "P"));
+            }
+        }
+
+        private void OnGroupData(object obj)
+        {
+            if (_dataView.GroupDescriptors.Count != 0)
+            {
+                _dataView.GroupDescriptors.Clear();
+            }
+            else
+            {
+                _dataView.GroupDescriptors.Add(new GroupDescriptor() { Member = nameof(Employee.Department) });
+            }
+        }
+    }
+}
